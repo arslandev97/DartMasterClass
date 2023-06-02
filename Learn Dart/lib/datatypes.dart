05cd8d24bdc3e27c2fe37c6a1bfdbacf49ea13ca
arslandev97 <arslandev97@gmail.com>
2023-06-02 23:34:58 +0500

    Upload Dart Course

---
+++ Learn Dart/lib/datatypes.dart
@@ -0,0 +1,59 @@
+void main() {
+  
+  // Data Types
+  // data types is a classification or categorization of data. 
+
+  // var number1= 1234564; // 4 byte
+  // var number2= 456;
+
+  // var firstName = "Arslan";
+  // var lastName = "Yousaf";
+
+  
+  // 1. Numbers ---> integers ---> Floating Point Numbers 
+  // int --> Integers
+
+  // int studentAge = 18.5; // Generate an error
+  // int studentAge = 18;
+  // print(studentAge);
+  // print(studentAge.runtimeType);
+  // print('Student Age is ${studentAge}');
+
+  
+  // double ---> Floating Point Values 
+  // double salary = 1000.80;
+  // print(salary);
+  // print(salary.runtimeType);
+  // print("Employee Salary is $salary");
+  // print("Employee Salary is ${salary}");
+
+  // double productPrice = 100;
+  // print(productPrice);
+
+
+  // 2. String ---> A string is a sequence of characters. 
+  //  you can create a string by enclosing a sequence of characters in single or double quotes.
+
+  // String personName = "Arslan";
+  // print("My Name is $personName");
+  // print("Welcome Mr.$personName & datatype is ${personName.runtimeType}");
+
+  // var EmployeeName = "Ali Raza";
+  // print(EmployeeName);
+  // print(EmployeeName.runtimeType);
+
+
+  // 3. bool ---> Booleans 
+  // A boolean is a data type that has two possible values: true and false.
+
+  // int marks = 61;
+  // bool result = marks > 33;
+  // bool result = 33 >= 33;
+  // bool hello = true;
+  // print(hello);
+  // print(result);
+  // print(result.runtimeType);
+
+
+
+}
---
+++ Learn Dart/lib/main.dart
@@ -0,0 +1,47 @@
+void main(){
+
+  // Run First Dart Program
+  print("Alhamdulillah Run First Dart Program!");
+
+  
+  // Print Statemen USing Single Quotes 
+  print('Welcome to Dart Flutter Course');
+
+
+  // Understand Dart Basic Syntax
+  // What is Syntax?
+  
+  // Example : print();
+  print("welcome");
+
+  // Error : Bug --> Debug 
+  // 1. Syntax Error
+
+  // print("Hello World!") // Syntax Error
+  // print("Hello World!");
+
+  
+  // 2. Logical Error
+  // Write a program in which Add two Numbers and print their sum 
+  // print('Sum = ${10 - 10}');
+
+
+  // 3. Run Time Error
+  // A runtime error is an error that occurs when a program you're using or writing crashes or produces a wrong output.
+  // print(10/0);
+
+
+  // Semicolon in Dart
+  // The semicolon is used to terminate the statement that means,it indicates the statement is ended here.
+
+  print("Hellow World!");
+  var firstName = "Arslan";
+  print(firstName);
+
+  // DartPad.dev
+  // Browser Base IDE : Integrated Development Envirement
+  // DartPad is an open source tool that lets you play with the Dart language in any modern browser.
+
+
+  
+}
