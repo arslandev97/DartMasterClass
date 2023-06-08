7767a1f8da27da76d9657bdc6d737a300340fd8d
arslandev97 <arslandev97@gmail.com>
2023-06-08 16:21:46 +0500

    update set file

--- Learn Dart/lib/09_sets.dart
+++ Learn Dart/lib/09_sets.dart
@@ -7,61 +7,61 @@ void main() {
 
 
   // how to create sets in dart
-  // var users = {"Arslan", "Hamza", "Muzammil", 'Amir'};
-  // print(users);
-  // print(users.runtimeType);
+  var users = {"Arslan", "Hamza", "Muzammil", 'Amir'};
+  print(users);
+  print(users.runtimeType);
 
   // Type : Set<Object>
-  // var persons = {1, 2, "Ibraheem", "Mehroz", "Ali"};
-  // print(persons);
-  // print(persons.runtimeType);
+  var persons = {1, 2, "Ibraheem", "Mehroz", "Ali"};
+  print(persons);
+  print(persons.runtimeType);
 
 
   // Duplicates are not allowed in sets
-  // var numbers = {1,2,3,4,5,6,7,8,9,0, 5};
-  // print(numbers);
-  // print(numbers.length);
-  // print(numbers.runtimeType);
+  var numbers = {1,2,3,4,5,6,7,8,9,0, 5};
+  print(numbers);
+  print(numbers.length);
+  print(numbers.runtimeType);
 
   
   // Create set useing SET keyword
 
-  // Set country = {"Pakistan", "Sudia Arabia", "UAE"}; 
-  // print(country);
-  // print(country.length);
-  // print(country.runtimeType);
+  Set country = {"Pakistan", "Sudia Arabia", "UAE"}; 
+  print(country);
+  print(country.length);
+  print(country.runtimeType);
 
 
   // Create Empty Set using SET Constructor
-  // var emptySet = Set();
-  // print(emptySet);
-  // print(emptySet.runtimeType);
+  var myEmptySet = Set();
+  print(myEmptySet);
+  print(myEmptySet.runtimeType);
 
 
   // Create Empty Set using SET Literal
-  // var emptySet = <String>{};
-  // print(emptySet);
-  // print(emptySet.runtimeType);
+  var emptySet = <String>{};
+  print(emptySet);
+  print(emptySet.runtimeType);
 
 
   // Type Specific
-  // Set <int> counting = {};
-  // print(counting);
-  // print(counting.runtimeType);
+  Set <int> counting = {};
+  print(counting);
+  print(counting.runtimeType);
 
   
   // How to add new data into sets
-  // counting.add(0);
-  // counting.add(01);
-  // counting.add(5);
-  // counting.add(7);
-  // counting.add(8);
-  // print(counting); // output: {0, 1, 5, 7, 8}
+  counting.add(0);
+  counting.add(01);
+  counting.add(5);
+  counting.add(7);
+  counting.add(8);
+  print(counting); // output: {0, 1, 5, 7, 8}
 
 
   // Create String type Empty Set
-  Set <String> emptySet =  {};
-  print(emptySet);
+  // Set <String> emptySet =  {};
+  // print(emptySet);
 
   
   // Add multiple items to the set
@@ -77,54 +77,53 @@ void main() {
   // emptySet.removeWhere((element) => element.length < 6);
   // print(emptySet);
 
-  // Set <int> counting = {1,2,3,4,5,6,7,8,9,0};
-  // print(counting);
+  Set <int> countingNumber = {1,2,3,4,5,6,7,8,9,0};
+  print(countingNumber);
 
-  // counting.removeWhere((element) => element %2 == 1);
-  // print(counting);
+  countingNumber.removeWhere((element) => element %2 == 1);
+  print(countingNumber);
 
   // clear set
-  // emptySet.clear();
-  // print(emptySet);
+  emptySet.clear();
+  print(emptySet);
 
   // Contain()
-  // print(counting.contains(31));
+  print(countingNumber.contains(31));
 
   // Set Properties
-  /*
-    1. length
-    2. first
-    3. last
-    4. isEmpty
-    5. isNotEmpty
-  
-   */
 
+  print(counting.first);
+  print(counting.last);
+  print(counting.isEmpty);
+  print(counting.isNotEmpty);
+  print(counting.length);
 
   // Set Methods
 
-  // List <int> numbers_counting = counting.toList();
-  // print(numbers_counting);
-  // print(numbers_counting.runtimeType);
+  List <int> numbers_counting = counting.toList();
+  print(numbers_counting);
+  print(numbers_counting.runtimeType);
 
 
   // Union
 
-  // Set <int> set1 = {1,2,3};
-  // Set <int> set2 = {4,5,6, 1};
+  Set <int> set1 = {1,2,3};
+  Set <int> set2 = {4,5,6, 1};
 
-  // Set <int> unionSet = set1.union(set2);
-  // print(unionSet);
-  // print(unionSet.runtimeType);
+  Set <int> unionSet = set1.union(set2);
+  print(unionSet);
+  print(unionSet.runtimeType);
 
 
   // intersection
 
-  Set <int> set1 = {1,2,3};
-  Set <int> set2 = {4,5,3};
-
   Set <int> intersectionSet = set1.where((element) => set2.contains(element)).toSet();
   print(intersectionSet);
+
+
+   // Subtracting
+  Set <int> subtractingSet = set1.difference(set2);
+  print(subtractingSet);
 
   
 }
